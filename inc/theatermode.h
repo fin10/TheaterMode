b42517261fc2129596854feb17218cfb219e8c6e
fin10 <fender64@gmail.com>
2016-08-27 13:40:12 +0900

    upload last full-codes

---
+++ inc/theatermode.h
@@ -0,0 +1,13 @@
+#ifndef __theatermode_H__
+#define __theatermode_H__
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "theatermode"
+
+#if !defined(PACKAGE)
+#define PACKAGE "com.fin10.tizen.theatermode"
+#endif
+
+#endif /* __theatermode_H__ */
