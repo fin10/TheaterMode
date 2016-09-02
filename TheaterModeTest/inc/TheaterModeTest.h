810bc75974aab5cdfe3eaab8f98a574286d8921d
fin10 <fender64@gmail.com>
2016-09-03 00:52:55 +0900

    add project for test

---
+++ TheaterModeTest/inc/TheaterModeTest.h
@@ -0,0 +1,13 @@
+#ifndef __TheaterModeTest_H__
+#define __TheaterModeTest_H__
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "TheaterModeTest"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.theatermodetest"
+#endif
+
+#endif /* __TheaterModeTest_H__ */
